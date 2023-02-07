fb742f029273ce8dc10f47b36b472a4e8e0be725
zaidzaidan <zaidzaidan79@gmail.com>
2023-02-08 01:49:51 +0200

    cta and possibility

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -3,7 +3,15 @@ import Styles from "./style.module.css"
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className={Styles.gpt_cta}>
+      <div className={Styles.gpt_cta_content}>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className={Styles.gpt_cta_button}>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import Styles from "./style.module.css"
-
+import React from "react";
+import Styles from "./style.module.css";
+import image from "../../assets/Feature Image.png";
 const Possiblity = () => {
   return (
-    <div>Possiblity</div>
-  )
-}
+    <div className={Styles.gpt_Possibility + " " + Styles.section_padding}>
+      <div className={Styles.gpt_Possibility_image}>
+        <img src={image} alt="Possibility" />
+      </div>
+      <div className={Styles.gpt_Possibility_content}>
+        <h4 className={Styles.firstH4}>Request Early Access to Get Started</h4>
+        <h1>The possibilities are beyond your imagination</h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4 className={Styles.secondH4}>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
+};
 
-export default Possiblity
+export default Possiblity;
